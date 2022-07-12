cd98c06a340dba45b79e2579a0860e6232babf5b
marco-vit-pagopa <marco.vit@pagopa.it>
2022-07-12 18:27:47 +0200

    PN-1394 aggiunta env dev

---
+++ cd-cli/custom-config/pn-frontend/compile_envs/pn-personafisica-login.sh
@@ -0,0 +1,7 @@
+export REACT_APP_ENABLE_ASSISTANCE="true"
+export REACT_APP_PAGOPA_HELP_EMAIL="assistenza@pn.it"
+export REACT_APP_SPID_CIE_ENTITY_ID="xx_servizicie_test"
+export REACT_APP_SPID_TEST_ENV_ENABLED="true"
+export REACT_APP_URL_API_LOGIN="https://hub-login.ecs.dev.pn.pagopa.it"
+export REACT_APP_URL_FILE_PRIVACY_DISCLAIMER="#privacy-disclaimer"
+export REACT_APP_URL_FILE_TERMS_AND_CONDITIONS="#terms-and-conditions"
