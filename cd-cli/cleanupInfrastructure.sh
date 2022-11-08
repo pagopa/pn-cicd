33249225a336c65646c96a9909df82ab381d6e39
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2022-11-08 17:38:33 +0100

    PN-2565: infrastructure cleanup (log retention)

---
+++ cd-cli/cleanupInfrastructure.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+    
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+
+usage() {
+      cat <<EOF
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type>
+
+    [-h]                      : this help message
+    [-v]                      : verbose mode
+    [-p <aws-profile>]        : aws cli profile (optional)
+    -r <aws-region>           : aws region as eu-south-1
+    -e <env-type>             : one of dev / uat / svil / coll / cert / prod
+    
+EOF
+  exit 1
+}
+
+parse_params() {
+  # default values of variables set from params
+  project_name=pn
+  work_dir=$HOME/tmp/deploy
+  aws_profile=""
+  aws_region=""
+  env_type=""
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    -p | --profile) 
+      aws_profile="${2-}"
+      shift
+      ;;
+    -r | --region) 
+      aws_region="${2-}"
+      shift
+      ;;
+    -e | --env-name) 
+      env_type="${2-}"
+      shift
+      ;;
+    -w | --work-dir) 
+      work_dir="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  # check required params and arguments
+  [[ -z "${env_type-}" ]] && usage 
+  [[ -z "${aws_region-}" ]] && usage
+  return 0
+}
+
+dump_params(){
+  echo ""
+  echo "######      PARAMETERS      ######"
+  echo "##################################"
+  echo "Project Name:       ${project_name}"
+  echo "Work directory:     ${work_dir}"
+  echo "Env Name:           ${env_type}"
+  echo "AWS region:         ${aws_region}"
+  echo "AWS profile:        ${aws_profile}"
+}
+
+
+# START SCRIPT
+
+parse_params "$@"
+dump_params
+
+
+cd $work_dir
+
+echo ""
+echo "=== Base AWS command parameters"
+aws_command_base_args=""
+if ( [ ! -z "${aws_profile}" ] ) then
+  aws_command_base_args="${aws_command_base_args} --profile $aws_profile"
+fi
+if ( [ ! -z "${aws_region}" ] ) then
+  aws_command_base_args="${aws_command_base_args} --region  $aws_region"
+fi
+echo ${aws_command_base_args}
+
+logGroups=$(aws ${aws_command_base_args} logs describe-log-groups --output json | jq '.logGroups |  map(select(.retentionInDays == null)) | map(.logGroupName)')
+echo $logGroups
+
+updateRetention(){
+  echo "Setting retentiong on $1 to $2 days"
+  aws ${aws_command_base_args} logs put-retention-policy --log-group-name $1 --retention-in-days $2 
+}
+
+for logGroup in $(echo "${logGroups}" | jq -r '.[]'); do
+  updateRetention $logGroup 14
+done
