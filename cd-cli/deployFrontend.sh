e4ded183b56f376412af30595bfa206e440e6c1c
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-02-20 15:18:02 +0100

    PN-152: fix unbounded variable

--- cd-cli/deployFrontend.sh
+++ cd-cli/deployFrontend.sh
@@ -208,20 +208,6 @@ echo "====================================================================="
 echo "====================================================================="
 
 function prepareOneCloudFront() {
-
-  echo ""
-  echo "=== Create Logs Bucket ${CdnName}"
-  aws ${aws_log_base_args} \
-    cloudformation deploy \
-      --stack-name $CdnName-logging \
-      --template-file pn-frontend/aws-cdn-templates/one-logging.yaml
-
-  logBucketName=$( aws ${aws_command_base_args} \
-    cloudformation describe-stacks \
-      --stack-name $CdnName-logging \
-      --output json \
-  | jq -r ".Stacks[0].Outputs | .[] | select( .OutputKey==\"LogsBucketName\") | .OutputValue" )
-
   CdnName=$1
   WebDomain=$2
   WebCertificateArn=$3
@@ -239,6 +225,20 @@ function prepareOneCloudFront() {
   if ( [ ! -z "$HAS_MONITORING" ]) then
     OptionalParameters="${OptionalParameters} AlarmSNSTopicArn=${AlarmSNSTopicArn}"
   fi
+
+  echo ""
+  echo "=== Create Logs Bucket ${CdnName}"
+  aws ${aws_log_base_args} \
+    cloudformation deploy \
+      --stack-name $CdnName-logging \
+      --template-file pn-frontend/aws-cdn-templates/one-logging.yaml
+
+  logBucketName=$( aws ${aws_command_base_args} \
+    cloudformation describe-stacks \
+      --stack-name $CdnName-logging \
+      --output json \
+  | jq -r ".Stacks[0].Outputs | .[] | select( .OutputKey==\"LogsBucketName\") | .OutputValue" )
+
 
   echo ""
   echo "=== Create CDN ${CdnName} with domain ${WebDomain} in zone ${HostedZoneId}"
