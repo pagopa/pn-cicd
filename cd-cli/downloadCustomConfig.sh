0e8d3f176b12a513db469c3c7a70ffbd7cf8b208
mbrachi <matteo.brachi@pagopa.it>
2023-04-26 11:30:35 +0200

    PN-5103:Modify script for create desiredCommitIds.sh from repo pn-configuration

--- cd-cli/downloadCustomConfig.sh
+++ cd-cli/downloadCustomConfig.sh
@@ -13,7 +13,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
       cat <<EOF
-    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> [-s <cfgRepositorySecretName>]
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> [-s <cfgRepositorySecretName>] [-c <PnConfigurationTag>]
 
     [-h]                           : this help message
     [-v]                           : verbose mode
@@ -22,6 +22,8 @@ usage() {
     -e <env-type>                  : one of dev / uat / svil / coll / cert / prod
     [-s <cfgRepositorySecretName>] : nome del secret che contiene le informazioni per effettuare
                                      il download delle configurazioni specifiche dell'ambiente
+    [-c <PnConfigurationTag>]      : a partire dal file repository-list.json nel repo pn-configuration 
+                                     costruisce il file desiredCommitIds.sh
     
     
 EOF
@@ -36,6 +38,8 @@ parse_params() {
   aws_region=""
   env_type=""
   configuration_repository_secret_name="pn-configurations-repository"
+  PN_CONFIGURATION_TAG=""
+
 
   while :; do
     case "${1-}" in
@@ -59,6 +63,10 @@ parse_params() {
       ;;
     -w | --work-dir) 
       work_dir="${2-}"
+      shift
+      ;;
+    -c | --configuration) 
+      PN_CONFIGURATION_TAG="${2-}"
       shift
       ;;
     -?*) die "Unknown option: $1" ;;
@@ -85,6 +93,7 @@ dump_params(){
   echo "AWS region:        ${aws_region}"
   echo "AWS profile:       ${aws_profile}"
   echo "Configuration Repository Informations contained into Secret: ${configuration_repository_secret_name}"
+  echo "Configuration CommitIds: ${PN_CONFIGURATION_TAG}"
 }
 
 
@@ -135,4 +144,58 @@ else
   echo "=== Nothing to do!!" 
   mkdir custom-config
   touch custom-config/empty.txt
+fi
+
+PN_CONFIGURATION_TAG_param=""
+SUB1=tag
+
+if ( [ ! -z "${PN_CONFIGURATION_TAG}"  ] ) ; then
+  PN_CONFIGURATION_TAG_param=""
+  SUB1=tag
+  echo PN_CONFIGURATION_TAG is present. 
+  touch desiredCommitIds.sh
+  #List Components form PN-CONFIGURATION:
+  for PN_CONFIGURATION_TAG_param in $(curl -s https://raw.githubusercontent.com/pagopa/pn-configuration/$PN_CONFIGURATION_TAG/repository-list.json  |  jq 'keys_unsorted'  | grep -E "Id|Url" | sed -E 's/"//g' | sed -E 's/,//g' | sed -E 's/ //g');do
+  #List of CommitsIds (tag or branch):
+  PN_COMMIT=$(echo "$( curl -s https://raw.githubusercontent.com/pagopa/pn-configuration/$PN_CONFIGURATION_TAG/repository-list.json | jq -r '.'\"$PN_CONFIGURATION_TAG_param\"'' )") ;
+
+  #ONLY FOR TEST,
+  #for PN_CONFIGURATION_TAG_param in $(cat example.json |  jq 'keys_unsorted'  | grep -E "Id|Url" | sed -E 's/"//g' | sed -E 's/,//g' | sed -E 's/ //g'); do
+  #PN_COMMIT=$(echo "$( cat example.json | jq -r '.'\"$PN_CONFIGURATION_TAG_param\"'' )"); 
+  #END TEST
+
+  #ImageURL and Commit
+  LineNum=$(echo $PN_COMMIT | wc -c)
+  if [ 40 -le "$LineNum" ] ; then
+  echo "CommitID or ImageUrl is present $PN_CONFIGURATION_TAG_param";
+  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT" >> desiredCommitIds.sh
+
+  #TAG:
+  elif grep -q "$SUB1" <<< "$PN_COMMIT"; then
+  echo "TAG is present for $PN_CONFIGURATION_TAG_param";
+  #take only tag es: v1.0.0:
+  TAG=$(echo $PN_COMMIT | cut -d "/" -f 2)
+  #declare variable for repo:
+  REPO=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_commitId//g' | sed -E 's/_/-/g')
+  PN_COMMIT_ID=$(echo "$( curl -L -s  \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/pagopa/$REPO/tags | jq '.[] | select(.name=='\"$TAG\"') ' |  jq -r '.commit.sha' )") ;
+  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desiredCommitIds.sh
+
+  #BRANCH
+  else
+   echo "BRANCH is present for $PN_CONFIGURATION_TAG_param";
+  #declare variable for repo:
+  REPO=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_commitId//g' | sed -E 's/_/-/g')
+  PN_COMMIT_ID=$(echo "$( curl -L -s \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/pagopa/$REPO/branches/$PN_COMMIT |  jq -r '.commit.sha' )") ;
+  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desiredCommitIds.sh
+  fi
+  done
+  echo export completed
+  else
+  echo "nothing to do"
 fi
