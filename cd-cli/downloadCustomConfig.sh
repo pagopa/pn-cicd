472fb1447941b38e057de4f26da09dfcc5f5a674
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-04-27 12:39:04 +0200

    PN-5103: added cicd account

--- cd-cli/downloadCustomConfig.sh
+++ cd-cli/downloadCustomConfig.sh
@@ -13,7 +13,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
       cat <<EOF
-    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> [-s <cfgRepositorySecretName>] [-c <PnConfigurationTag>]
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> [-s <cfgRepositorySecretName>] [-c <PnConfigurationTag>] [-i <CiCdAccountId>]
 
     [-h]                           : this help message
     [-v]                           : verbose mode
@@ -24,6 +24,7 @@ usage() {
                                      il download delle configurazioni specifiche dell'ambiente
     [-c <PnConfigurationTag>]      : a partire dal file repository-list.json nel repo pn-configuration 
                                      costruisce il file desiredCommitIds.sh
+    [-i <CiCdAccountId>]            : CICD Account Id
     
     
 EOF
@@ -39,7 +40,7 @@ parse_params() {
   env_type=""
   configuration_repository_secret_name="pn-configurations-repository"
   PN_CONFIGURATION_TAG=""
-
+  cicd_account_id=""
 
   while :; do
     case "${1-}" in
@@ -69,6 +70,10 @@ parse_params() {
       PN_CONFIGURATION_TAG="${2-}"
       shift
       ;;
+    -i | --cicd) 
+      cicd_account_id="${2-}"
+      shift
+      ;;
     -?*) die "Unknown option: $1" ;;
     *) break ;;
     esac
@@ -94,6 +99,7 @@ dump_params(){
   echo "AWS profile:       ${aws_profile}"
   echo "Configuration Repository Informations contained into Secret: ${configuration_repository_secret_name}"
   echo "Configuration CommitIds: ${PN_CONFIGURATION_TAG}"
+  echo "CICD Account ID:   ${cicd_account_id}"
 }
 
 
@@ -116,6 +122,11 @@ if ( [ ! -z "${aws_region}" ] ) then
 fi
 echo ${aws_command_base_args}
 
+aws_cicd_command_base_args=""
+if ( [ ! -z "${aws_profile}" ] ) then
+  aws_cicd_command_base_args="${aws_cicd_command_base_args} --profile $aws_profile"
+fi
+echo "CICD command ${aws_cicd_command_base_args}"
 
 if ( [ ! -z "${configuration_repository_secret_name}" ] ) then 
   secretPresent=$( aws ${aws_command_base_args} secretsmanager list-secrets \
@@ -150,7 +161,7 @@ PN_CONFIGURATION_TAG_param=""
 SUB1=tag
 
 
-if ( [ ! -z "${PN_CONFIGURATION_TAG}"  ] ) ; then
+if ( [ ! -z "${PN_CONFIGURATION_TAG}" && ! -z "${cicd_account_id}" ] ) ; then
   echo PN_CONFIGURATION_TAG is present. 
   PN_CONFIGURATION_TAG_param=""
   SUB1=tag
@@ -162,57 +173,59 @@ if ( [ ! -z "${PN_CONFIGURATION_TAG}"  ] ) ; then
    
   #Take list of all components in json file: 
   for PN_CONFIGURATION_TAG_param in $( cat repository-list.json |  jq 'keys_unsorted'  | grep -E "Id|Url" | sed -E 's/"//g' | sed -E 's/,//g' | sed -E 's/ //g' ); do
-  #Take for all components tag, branch, ImageUrl ecc...
-  PN_COMMIT=$(echo "$( cat repository-list.json | jq -r '.'\"$PN_CONFIGURATION_TAG_param\"'' )"); 
+    #Take for all components tag, branch, ImageUrl ecc...
+    PN_COMMIT=$(echo "$( cat repository-list.json | jq -r '.'\"$PN_CONFIGURATION_TAG_param\"'' )"); 
 
-  #AWS ECR Image:
-  if grep -q "$SUB2" <<< "$PN_COMMIT"; then
-  echo "IMAGE is present."
-  REPOIMAGE=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_imageUrl//g' | sed -E 's/_/-/g');
-  TAGIMAGE=$(echo $PN_COMMIT | cut -d "@" -f 2);
-  SHAIMAGE=$(aws ecr describe-images --repository-name $REPOIMAGE --region eu-central-1 --image-ids imageTag="$TAGIMAGE" | jq -r .imageDetails | jq -r '.[0]' | jq -r '.imageDigest')
-  PN_COMMIT_ID=$(echo $PN_COMMIT | sed -E "s|$TAGIMAGE|$SHAIMAGE|g" )
-  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desired-commit-ids-env.sh
+    #AWS ECR Image:
+    if grep -q "$SUB2" <<< "$PN_COMMIT"; then
+      echo "IMAGE is present."
+      REPOIMAGE=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_imageUrl//g' | sed -E 's/_/-/g');
+      TAGIMAGE=$(echo $PN_COMMIT | cut -d "@" -f 2);
+      SHAIMAGE=$(aws ${aws_cicd_command_base_args} ecr describe-images --repository-name $REPOIMAGE --region eu-central-1 --image-ids imageTag="$TAGIMAGE" --registry-id="$cicd_account_id" | jq -r .imageDetails | jq -r '.[0]' | jq -r '.imageDigest')
+      PN_COMMIT_ID=$(echo $PN_COMMIT | sed -E "s|$TAGIMAGE|$SHAIMAGE|g" )
+      echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desired-commit-ids-env.sh
 
-  #TAG:
-  elif grep -q "$SUB1" <<< "$PN_COMMIT"; then
-  echo "TAG is present for $PN_CONFIGURATION_TAG_param";
-  #take only tag es: v1.0.0:
-  TAG=$(echo $PN_COMMIT | cut -d "/" -f 2)
-  #declare variable for repo:
-  REPO=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_commitId//g' | sed -E 's/_/-/g')
-  PN_COMMIT_ID=$(echo "$( curl -L -s  \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/pagopa/$REPO/tags | jq '.[] | select(.name=='\"$TAG\"') ' |  jq -r '.commit.sha' )") ;
-  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desired-commit-ids-env.sh
-  
-  #CommitID (nothing to do):
-  elif  [ $(echo  $PN_COMMIT | wc -c) -eq 41 ] ; then
-  echo "CommitID is present $PN_CONFIGURATION_TAG_param";
-  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT" >> desired-commit-ids-env.sh
+    #TAG:
+    elif grep -q "$SUB1" <<< "$PN_COMMIT"; then
+      echo "TAG is present for $PN_CONFIGURATION_TAG_param";
+      #take only tag es: v1.0.0:
+      TAG=$(echo $PN_COMMIT | cut -d "/" -f 2)
+      #declare variable for repo:
+      REPO=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_commitId//g' | sed -E 's/_/-/g')
+      PN_COMMIT_ID=$(echo "$( curl -L -s  \
+      -H "Accept: application/vnd.github+json" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/repos/pagopa/$REPO/tags | jq '.[] | select(.name=='\"$TAG\"') ' |  jq -r '.commit.sha' )") ;
+      echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desired-commit-ids-env.sh
+    
+    #CommitID (nothing to do):
+    elif  [ $(echo  $PN_COMMIT | wc -c) -eq 41 ] ; then
+      echo "CommitID is present $PN_CONFIGURATION_TAG_param";
+      echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT" >> desired-commit-ids-env.sh
 
-  #BRANCH
-  else
-  echo "BRANCH is present for $PN_CONFIGURATION_TAG_param";
-  #declare variable for repo:
-  REPO=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_commitId//g' | sed -E 's/_/-/g')
-  PN_COMMIT_ID=$(echo "$( curl -L -s \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/pagopa/$REPO/branches/$PN_COMMIT |  jq -r '.commit.sha' )") ;
-  echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desired-commit-ids-env.sh
-  fi
+    #BRANCH
+    else
+      echo "BRANCH is present for $PN_CONFIGURATION_TAG_param";
+      #declare variable for repo:
+      REPO=$(echo $PN_CONFIGURATION_TAG_param | sed -E 's/_commitId//g' | sed -E 's/_/-/g')
+      PN_COMMIT_ID=$(echo "$( curl -L -s \
+      -H "Accept: application/vnd.github+json" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/repos/pagopa/$REPO/branches/$PN_COMMIT |  jq -r '.commit.sha' )") ;
+      echo "export $PN_CONFIGURATION_TAG_param=$PN_COMMIT_ID" >> desired-commit-ids-env.sh
+    fi
   done
   echo export completed
-  else
+
+  cd ../..
+  mkdir -p custom-config
+  cp -r pn-configuration/${env_type}/* custom-config/
+
+  chmod a+x ./desired-commit-ids-env.sh
+   . ./desired-commit-ids-env.sh
+   
+else
   echo "nothing to do"
 fi
 
 
-cd ../..
-  mkdir -p custom-config
-  cp -r * custom-config/
-
-  chmod a+x ./desired-commit-ids-env.sh
-   . ./desired-commit-ids-env.sh
