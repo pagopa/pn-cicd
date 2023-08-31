7d22f97474039a980ab2d61498bc665488cdf676
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-08-31 11:55:02 +0200

    PN-7542: fix unbounded variable pn_microsvc_commitid

--- cd-cli/deployEcsService.sh
+++ cd-cli/deployEcsService.sh
@@ -249,10 +249,10 @@ echo " - Copy Lambdas zip"
 lambdasZip='functions.zip'
 lambdasLocalPath='functions'
 
-functionsDirPresent=$( ( aws ${aws_command_base_args} --endpoint-url https://s3.eu-central-1.amazonaws.com s3api head-object --bucket ${LambdasBucketName} --key "${microcvs_name}/commits/${pn_microsvc_commitId}/${lambdasZip}" 2> /dev/null > /dev/null ) && echo "OK"  || echo "KO" )
+functionsDirPresent=$( ( aws ${aws_command_base_args} --endpoint-url https://s3.eu-central-1.amazonaws.com s3api head-object --bucket ${LambdasBucketName} --key "${microcvs_name}/commits/${pn_microsvc_commitid}/${lambdasZip}" 2> /dev/null > /dev/null ) && echo "OK"  || echo "KO" )
 if ( [ $functionsDirPresent = "OK" ] ) then
   aws ${aws_command_base_args} --endpoint-url https://s3.eu-central-1.amazonaws.com s3api get-object \
-        --bucket "$LambdasBucketName" --key "${microcvs_name}/commits/${pn_microsvc_commitId}/${lambdasZip}" \
+        --bucket "$LambdasBucketName" --key "${microcvs_name}/commits/${pn_microsvc_commitid}/${lambdasZip}" \
         "${lambdasZip}"
 
   unzip ${lambdasZip} -d ./${lambdasLocalPath}
