f771d44721c25df0b169a6ab126b6e5ba28fe681
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-04-16 17:01:21 +0200

    PN-10326: runtime variables file creation

---
+++ cd-cli/commons/runtime-env-file-creation.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+
+usage() {
+      cat <<EOF
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] -p <project-name> -r <aws-region> -m <microcvs-name>
+    [-h]                      : this help message
+    -p <project-name>         : project name
+    -r <aws-region>           : aws region
+    -m <microcvs-name>        : microcvs name
+
+EOF
+  exit 1
+}
+
+parse_params() {
+  # default values of variables set from params
+  project_name=$HOME
+  aws_region=""
+  microcvs_name=""
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -p | --project-name)
+      project_name="${2-}"
+      shift
+      ;;
+    -r | --aws-region)
+      aws_region="${2-}"
+      shift
+      ;;
+    -m | --microcvs-name)
+      microcvs_name="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+   # check required params and arguments
+  [[ -z "${project_name-}" ]] && usage
+  [[ -z "${aws_region-}" ]] && usage
+  [[ -z "${microcvs_name-}" ]] && usage
+  return 0
+}
+
+dump_params(){
+  echo ""
+  echo "######      PARAMETERS      ######"
+  echo "##################################"
+  echo "Project Name:                ${project_name}"
+  echo "AWS Region:                  ${aws_region}"
+  echo "Microservice Name:           ${microcvs_name}"
+}
+
+account_id=$(aws sts get-caller-identity --query Account --output text)
+bucket_env_path=${project_name}-runtime-environment-variables-${aws_region}-${account_id}
+file_env_name="runtime-variable.env"
+
+if aws s3api head-object --bucket ${bucket_env_path} --key ${microcvs_name}/${file_env_name} > /dev/null 2>&1; then
+    echo "File ${file_env_name} already exists."
+else
+  touch ./${file_env_name}
+  echo "Generating ${file_env_name}"
+  aws s3 cp ${file_env_name} s3://${bucket_env_path}/${microcvs_name}/
+  rm ./${file_env_name}
+fi
