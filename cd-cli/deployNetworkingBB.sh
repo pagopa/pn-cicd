990ccba054e216642931249406c86002cb7d3081
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-08-21 14:33:00 +0200

    PN-12112: fix deployNetworking

--- cd-cli/deployNetworkingBB.sh
+++ cd-cli/deployNetworkingBB.sh
@@ -13,16 +13,17 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 usage() {
       cat <<EOF
-    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> -i <github-commitid> -a <account-type> [-c <custom_config_dir>]
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> -i <github-pn-confinfo-bb-commitid> -m <github-pn-infra-commitid> -a <account-type> [-c <custom_config_dir>]
 
-    [-h]                      : this help message
-    [-v]                      : verbose mode
-    [-p <aws-profile>]        : aws cli profile (optional)
-    -r <aws-region>           : aws region as eu-south-1
-    -e <env-type>             : one of dev / uat / svil / coll / cert / prod
-    -i <github-commitid>      : commitId for github repository pagopa/pn-infra-confinfo-bb
-    [-c <custom_config_dir>]  : where tor read additional env-type configurations
-    -a <account-type>         : account type, can be core or confinfo
+    [-h]                                     : this help message
+    [-v]                                     : verbose mode
+    [-p <aws-profile>]                       : aws cli profile (optional)
+    -r <aws-region>                          : aws region as eu-south-1
+    -e <env-type>                            : one of dev / uat / svil / coll / cert / prod
+    -i <github-pn-confinfo-bb-commitid>      : commitId for github repository pagopa/pn-infra-confinfo-bb
+    -m <github-pn-infra-commitid>            : commitId for github repository pagopa/pn-infra
+    [-c <custom_config_dir>]                 : where tor read additional env-type configurations
+    -a <account-type>                        : account type, can be core or confinfo
     
 EOF
   exit 1
@@ -59,7 +60,11 @@ parse_params() {
       account_type="${2-}"
       shift
       ;;      
-    -i | --infra-commitid) 
+    -i | --pn-confinfo-bb-commitid) 
+      pn_confinfo_bb_commitid="${2-}"
+      shift
+      ;;
+    -m | --infra-commitid) 
       pn_infra_commitid="${2-}"
       shift
       ;;
@@ -81,9 +86,9 @@ parse_params() {
 
   # check required params and arguments
   [[ -z "${env_type-}" ]] && usage 
+  [[ -z "${pn_confinfo_bb_commitid-}" ]] && usage
   [[ -z "${pn_infra_commitid-}" ]] && usage
   [[ -z "${aws_region-}" ]] && usage
-  [[ -z "${pn_infra_commitid-}" ]] && usage
   [[ -z "${account_type-}" ]] && usage 
   return 0
 }
@@ -92,14 +97,15 @@ dump_params(){
   echo ""
   echo "######      PARAMETERS      ######"
   echo "##################################"
-  echo "Project Name:      ${project_name}"
-  echo "Work directory:    ${work_dir}"
-  echo "Custom config dir: ${custom_config_dir}"
-  echo "Infra CommitId:    ${pn_infra_commitid}"
-  echo "Account Type:      ${account_type}"
-  echo "Env Name:          ${env_type}"
-  echo "AWS region:        ${aws_region}"
-  echo "AWS profile:       ${aws_profile}"
+  echo "Project Name:                  ${project_name}"
+  echo "Work directory:                ${work_dir}"
+  echo "Custom config dir:             ${custom_config_dir}"
+  echo "Infra Confinfo bb CommitId:    ${pn_confinfo_bb_commitid}"
+  echo "Infra CommitId:                ${pn_infra_commitid}"
+  echo "Account Type:                  ${account_type}"
+  echo "Env Name:                      ${env_type}"
+  echo "AWS region:                    ${aws_region}"
+  echo "AWS profile:                   ${aws_profile}"
 }
 
 
@@ -136,9 +142,11 @@ export PATH="/usr/local/tfenv/bin:$PATH"
 
 ## Repository switch according to account type
 echo "=== Repository switch according to account type " 
-infra_repo="pn-infra-confinfo-bb"
+infra_confinfo_bb_repo="pn-infra-confinfo-bb"
+infra_repo="pn-infra"
 terraform_output_prefix="Core_"
 
+## Download pn-infra
 echo "=== Download ${infra_repo}" 
 if ( [ ! -e ${infra_repo} ] ) then 
   git clone https://github.com/pagopa/${infra_repo}.git
@@ -148,28 +156,38 @@ echo ""
 echo "=== Checkout ${infra_repo} commitId=${pn_infra_commitid}"
 ( cd ${infra_repo} && git fetch && git checkout $pn_infra_commitid )
 
+## Download pn-infra-confinfo-bb
+echo "=== Download ${infra_confinfo_bb_repo}" 
+if ( [ ! -e ${infra_confinfo_bb_repo} ] ) then 
+  git clone https://github.com/pagopa/${infra_confinfo_bb_repo}.git
+fi
+
+echo ""
+echo "=== Checkout ${infra_confinfo_bb_repo} commitId=${pn_confinfo_bb_commitid}"
+( cd ${infra_confinfo_bb_repo} && git fetch && git checkout $pn_confinfo_bb_commitid )
+
 ## Build diagnostic Lambda
-if ( [ -f ${infra_repo}/functions/build_lambda.sh ] ) then
-	( cd ${infra_repo}/functions/ && ./build_lambda.sh )
+if ( [ -f ${infra_confinfo_bb_repo}/functions/build_lambda.sh ] ) then
+	( cd ${infra_confinfo_bb_repo}/functions/ && ./build_lambda.sh )
 fi
 
 ## Apply tf
-(cd ${infra_repo}/src/main && ./terraform.sh init ${env_type} && ./terraform.sh apply ${env_type} --auto-approve)
+(cd ${infra_confinfo_bb_repo}/src/main && ./terraform.sh init ${env_type} && ./terraform.sh apply ${env_type} --auto-approve)
 
 terraformOutputPath=terraform-${env_type}-cfg.json
 
 ## Output tf
-(cd ${infra_repo}/src/main && terraform output --json ) | jq 'to_entries[] | { (.key | sub("'${terraform_output_prefix}'" ; "")): .value.value | (if type=="string" then . else join(",") end ) }' | jq -s 'reduce .[] as $item ({}; . *= $item )' | jq -s '{ Parameters: .[0] }' | tee $terraformOutputPath
+(cd ${infra_confinfo_bb_repo}/src/main && terraform output --json ) | jq 'to_entries[] | { (.key | sub("'${terraform_output_prefix}'" ; "")): .value.value | (if type=="string" then . else join(",") end ) }' | jq -s 'reduce .[] as $item ({}; . *= $item )' | jq -s '{ Parameters: .[0] }' | tee $terraformOutputPath
 
 echo ""
 echo "= Read Terraform Output file"
 cat ${terraformOutputPath} 
 
 echo " - copy custom config"
-mkdir -p $custom_config_dir/${infra_repo}
-cp -p ${terraformOutputPath} $custom_config_dir/${infra_repo}/
+mkdir -p $custom_config_dir/${infra_confinfo_bb_repo}
+cp -p ${terraformOutputPath} $custom_config_dir/${infra_confinfo_bb_repo}/
 
-ParamFilePath=$custom_config_dir/${infra_repo}/${terraformOutputPath}
+ParamFilePath=$custom_config_dir/${infra_confinfo_bb_repo}/${terraformOutputPath}
 
 echo ""
 echo "=== Deploy microservice-cloudwatch-dashboard FOR $env_type ACCOUNT"
