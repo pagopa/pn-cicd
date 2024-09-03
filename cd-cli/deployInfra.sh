843ba1d26fe59a6820d35ef2bd0a233da69c054d
marco-vit-pagopa <marco.vit@pagopa.it>
2024-09-03 13:42:57 +0200

    feature: PN-12545: se è presente il file runtime-infra/celonis_exports.yaml tale template verrà deployato su francoforte

--- cd-cli/deployInfra.sh
+++ cd-cli/deployInfra.sh
@@ -607,6 +607,43 @@ else
 fi
 
 echo ""
+echo "=== Deploy PN-Celonis-Export FOR $env_type ACCOUNT ON FRANKFURT region"
+CELONIS_EXPORTS_STACK_FILE=pn-infra/runtime-infra/celonis_exporta.yaml
+
+if [[ -f "$CELONIS_EXPORTS_STACK_FILE" ]]; then
+    echo "$CELONIS_EXPORTS_STACK_FILE exists, updating pn-celonis-exports stack"
+
+    echo ""
+    echo "= Read Parameters file"
+    cat ${ParamFilePath} 
+
+    echo ""
+    echo "= Enanched parameters file"
+    jq -s "{ \"Parameters\": .[0] } * .[1]" ${INFRA_ALL_OUTPUTS_FILE} ${ParamFilePath} \
+      | jq -s ".[] | .Parameters" | sed -e 's/": "/=/' -e 's/^{$/[/' -e 's/^}$/,/' \
+      > ${EnanchedParamFilePath}
+    echo "${PipelineParams} ]" >> ${EnanchedParamFilePath}
+    cat ${EnanchedParamFilePath}
+
+    aws_command_celonis_args=" --region eu-central-1"
+    if ( [ ! -z "${aws_profile}" ] ) then
+      aws_command_celonis_args="${aws_command_celonis_args} --profile $aws_profile"
+    fi
+
+    aws ${aws_command_celonis_args} \
+        cloudformation deploy \
+          --stack-name pn-celonis-exports-$env_type \
+          --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+          --template-file ${CELONIS_EXPORTS_STACK_FILE} \
+          --tags Microservice=pn-celonis-exports \
+          --parameter-overrides file://$( realpath ${EnanchedParamFilePath} )
+else
+  echo "${CELONIS_EXPORTS_STACK_FILE} file doesn't exist, stack update skipped"
+fi
+
+
+
+echo ""
 echo "=== Deploy PN-Cost-Saving FOR $env_type ACCOUNT"
 COST_SAVING_STACK_FILE=pn-infra/runtime-infra/pn-cost-saving.yaml
 
