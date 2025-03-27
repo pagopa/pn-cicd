c722bd0d085df18b8424a0e202e437eee58341cc
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2025-03-27 22:50:25 +0100

    fix naming runtime environment

--- cd-cli/deployEcsService.sh
+++ cd-cli/deployEcsService.sh
@@ -361,7 +361,7 @@ echo "=== Prepare parameters for $microcvs_name microservice deployment in $env_
 
 app_env_file_sha="-"
 
-echo "Environment variables file creation"
+echo "Environment variables file upload"
 (sh ${cwdir}/commons/upload-files-runtime.sh -p ${project_name} -r ${aws_region} -m ${microcvs_name} -e ${env_type})
 
 file_env_application_path=${microcvs_name}/scripts/aws/cfn/application-${env_type}.env
