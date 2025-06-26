a1ff2ed29c3b901e3705b885c9abee5604e514e0
Francesco Bianchi <105631409+fbianchicodermine@users.noreply.github.com>
2025-06-26 11:05:49 +0200

    fix(PN-15235): fix folder env for showcase-site (#470)

--- cd-cli/deployShowcaseSite.sh
+++ cd-cli/deployShowcaseSite.sh
@@ -377,10 +377,19 @@ landingTooManyErrorsAlarmArn=${tooManyErrorsAlarmArn}
 
 # replace config files in build artifact
 replace_config() {
+  echo " === replace_config for env_type=$1"
+
   LocalFilePath=/tmp/config.json
   echo '{}' > $LocalFilePath
+
+  if ( [ $1 == 'dev' ] ) then
+    configRootPath=.
+  else
+    # relative path from "deploy" dir
+    configRootPath=../../pn-showcase-site
+  fi
   
-  jq -s ".[0] * .[1]" ./conf/config-$1.json ${LocalFilePath} > ./conf/config.json
+  jq -s ".[0] * .[1]" $configRootPath/conf/config-$1.json ${LocalFilePath} > ./conf/config.json
   rm -f ./conf/config-dev.json
 }
 
