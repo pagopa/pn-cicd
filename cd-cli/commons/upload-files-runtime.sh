c5bae8b8087262de0a31d96596a3d3c31e4f1c3b
ViVeniero <vincenzo.veniero@pagopa.it>
2026-04-16 15:32:21 +0200

    PN-18977: requests changes

--- cd-cli/commons/upload-files-runtime.sh
+++ cd-cli/commons/upload-files-runtime.sh
@@ -111,7 +111,7 @@ account_id=$(aws sts get-caller-identity --query Account --output text)
 bucket_env_path=${project_name}-runtime-environment-variables-${aws_region}-${account_id}
 file_env_application_path=${microcvs_name}/scripts/aws/cfn/application-${env_type}.env
 file_env_application_name="application-${app_env_file_sha}.env"
-echo "current directory2: $(pwd)"
+
 if [[ -f "${file_env_application_path}" ]]; then
   aws ${aws_command_base_args} \
       s3 cp ${file_env_application_path} s3://${bucket_env_path}/${runtime_microcvs_name}/${file_env_application_name}
--- cd-cli/deployEcsServiceBB.sh
+++ cd-cli/deployEcsServiceBB.sh
@@ -439,16 +439,16 @@ echo "=== Prepare parameters for $microcvs_name microservice deployment in $env_
 
 
 app_env_file_sha="-"
-
-echo "Environment variables file upload"
-bash ${cwdir}/commons/upload-files-runtime.sh -p ${project_name} -r ${aws_region} -m ${microcvs_name} -e ${env_type}
-
 file_env_application_path=${microcvs_name}/scripts/aws/cfn/application-${env_type}.env
 if [[ -f "${file_env_application_path}" ]]; then
+  echo " - application env file found, calculating sha256"
   app_env_file_sha=$(sha256sum ${file_env_application_path} | awk '{print $1}')
   echo ""
   echo ""
 fi
+
+echo "Environment variables file upload"
+bash ${cwdir}/commons/upload-files-runtime.sh -p ${project_name} -r ${aws_region} -m ${microcvs_name} -e ${env_type}
 
 PreviousOutputFilePath=${microcvs_name}-storage-${env_type}-out.json
 TemplateFilePath=${microcvs_name}/scripts/aws/cfn/microservice.yml
