9ab4501f484f84d9b3299fcbfc15d78e80b085e6
ViVeniero <vincenzo.veniero@pagopa.it>
2026-04-27 15:41:42 +0200

    PN-19653: fix SHA deploy ECS

--- cd-cli/deployEcsService.sh
+++ cd-cli/deployEcsService.sh
@@ -418,8 +418,13 @@ if [[ -f "${file_env_application_path}" ]]; then
 fi
 
 echo "Environment variables file upload"
-bash ${cwdir}/commons/upload-files-runtime.sh -p ${project_name} -r ${aws_region} -m ${microcvs_name} -e ${env_type} -s ${app_env_file_sha} 
-
+echo "Environment variables file upload"
+bash ${cwdir}/commons/upload-files-runtime.sh \
+   -p ${project_name} \
+   -r ${aws_region} \
+   -m ${microcvs_name} \
+   -e ${env_type} \
+   -s "${app_env_file_sha:-}"
 
 
 PreviousOutputFilePath=${microcvs_name}-storage-${env_type}-out.json
