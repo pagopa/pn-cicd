43ef653544a126bf5923ffaf362dd8608da7f12e
ViVeniero <vincenzo.veniero@pagopa.it>
2026-08-17 12:23:06 +0200

    PN-21085: oer-sla-validation-monitoring lambda path in deployOERush

--- cd-cli/deployOerDashboard.sh
+++ cd-cli/deployOerDashboard.sh
@@ -185,6 +185,8 @@ if ( [ -f pn-infra/runtime-infra/pn-oer-dashboard.yaml ] ) then
     #logsBucketName=$(cat $INFRA_ALL_OUTPUTS_FILE | jq -r '.LogsBucketName') 
     #echo "LogsBucketName=${logsBucketName}"
 
+    logsBucketName=''
+    
     LambdasBucketName=$(cat $INFRA_ALL_OUTPUTS_FILE | jq -r '.LambdasBucketName') 
     echo "LambdasBucketName=${LambdasBucketName}"
 
