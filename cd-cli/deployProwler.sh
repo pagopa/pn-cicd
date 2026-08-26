a1944e38c51431d79f58e5d6f6be2349ec5abd00
Matteo Brachi <matteo.brachi@pagopa.it>
2026-08-26 11:02:34 +0200

    modify prowler deploy for add alarms

--- cd-cli/deployProwler.sh
+++ cd-cli/deployProwler.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 usage() {
   cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> -i <github-commitid> -a <account> [-c <custom-config-dir>] [-w <work-dir>]
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-region> -e <env-type> -i <github-commitid> -a <account> -b <artifact-bucket> [-c <custom-config-dir>] [-w <work-dir>]
 
   -h                         Show this help message
   -v                         Enable verbose mode
@@ -13,6 +13,7 @@ Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-p <aws-profile>] -r <aws-regi
   -e <env-type>              Environment name
   -i <github-commitid>       pn-infra Git commit ID
   -a <account>               Account type: core or confinfo
+  -b <artifact-bucket>       S3 bucket used for nested CloudFormation templates
   -c <custom-config-dir>     Additional environment configuration directory
   -w <work-dir>              Working directory (default: /tmp)
 EOF
@@ -28,6 +29,7 @@ aws_region=""
 env_type=""
 pn_infra_commitid=""
 account=""
+artifact_bucket=""
 custom_config_dir=""
 work_dir="/tmp"
 aws_profile=""
@@ -41,6 +43,7 @@ while :; do
     -e | --env-name) env_type="${2-}"; shift ;;
     -i | --infra-commitid) pn_infra_commitid="${2-}"; shift ;;
     -a | --account) account="${2-}"; shift ;;
+    -b | --artifact-bucket) artifact_bucket="${2-}"; shift ;;
     -c | --custom-config-dir) custom_config_dir="${2-}"; shift ;;
     -w | --work-dir) work_dir="${2-}"; shift ;;
     -?*) die "Unknown option: $1" ;;
@@ -52,6 +55,7 @@ done
 [[ -z "$aws_region" ]] && usage
 [[ -z "$env_type" ]] && usage
 [[ -z "$pn_infra_commitid" ]] && usage
+[[ -z "$artifact_bucket" ]] && usage
 [[ "$account" != "core" && "$account" != "confinfo" ]] && die "Account must be 'core' or 'confinfo', got: $account"
 
 if [[ "$env_type" != "dev" && "$env_type" != "hotfix" ]]; then
@@ -76,6 +80,14 @@ if [[ -n "$aws_profile" ]]; then
   aws_command_base_args+=(--profile "$aws_profile")
 fi
 
+template_bucket_base_path="pn-infra/${pn_infra_commitid}"
+template_bucket_base_url="https://s3.${aws_region}.amazonaws.com/${artifact_bucket}/${template_bucket_base_path}/runtime-infra"
+if [[ "$account" == "core" ]]; then
+  aws "${aws_command_base_args[@]}" s3 cp pn-infra \
+    "s3://${artifact_bucket}/${template_bucket_base_path}" \
+    --recursive --exclude '.git/*' --quiet
+fi
+
 outputs_file="infra_all_outputs-${env_type}.json"
 if [[ "$account" == "core" ]]; then
   (cd "$script_dir/commons" && ./merge-infra-outputs-core.sh -r "$aws_region" -e "$env_type" -o "$work_dir/$outputs_file")
@@ -95,8 +107,15 @@ fi
 jq -s '
     {"Parameters": (.[0] | with_entries(select(.key == "PnCoreAwsAccountId" or .key == "ConfidentialInfoAccountId")))} * .[1]
   ' "$outputs_file" "$config_path" \
-  | jq -r --arg account "$account" --arg environment "$env_type" \
-      '.Parameters + {AccountType: $account, EnvironmentType: $environment} | to_entries | map("\(.key)=\(.value)")' \
+  | jq -r \
+      --arg account "$account" \
+      --arg environment "$env_type" \
+      --arg template_bucket_base_url "$template_bucket_base_url" \
+      '.Parameters + {
+        AccountType: $account,
+        EnvironmentType: $environment,
+        TemplateBucketBaseUrl: $template_bucket_base_url
+      } | to_entries | map("\(.key)=\(.value)")' \
       > "$enhanced_config_path"
 
 aws "${aws_command_base_args[@]}" cloudformation deploy \
