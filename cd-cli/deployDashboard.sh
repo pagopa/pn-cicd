1b15ad7d70db772f27e77d404e5f2fde050c1e8a
ViVeniero <vincenzo.veniero@pagopa.it>
2026-09-24 16:05:45 +0200

    PN-21542: macro for dashboard template

--- cd-cli/deployDashboard.sh
+++ cd-cli/deployDashboard.sh
@@ -235,7 +235,7 @@ if ( [ -f pn-infra/runtime-infra/pn-oer-dashboard.yaml ] ) then
       mv ${TmpFilePath} ${ParamFilePath}
     fi
 
-    OptionalParameters="${OptionalParameters}, \"ProjectName=${ProjectName}\",\"DataMonitoringBucketName=${dashboardTemplateBucketName}\""
+    OptionalParameters="${OptionalParameters}, \"ProjectName=${project_name}\",\"DataMonitoringBucketName=${dashboardTemplateBucketName}\""
 
     PipelineParams="\"Version=cd_scripts_commitId=${cd_scripts_commitId},pn_infra_commitId=${pn_infra_commitId}\",$OptionalParameters"
     EnanchedParamFilePath="pn-infra/runtime-infra/pn-oer-dashboard-${env_type}-enhanced-cfg.json"
